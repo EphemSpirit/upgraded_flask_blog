b9eef0badda453f6662f89a13bb67fea67538f80
Drew Hund <languageinvestigator@gmail.com>
2026-05-20 13:20:27 -0400

    fetch and display blog posts on home page

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+res = requests.get("https://api.npoint.io/8a2f0b7cba1b18c6eaea")
+res.raise_for_status()
+
+posts = res.json()
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", posts=posts)
 
 
 @app.route("/about")
